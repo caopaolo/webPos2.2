44f6ceec24737d43c3b6a27196e4dc0a947357f1
Cao Pao Lo <caoanh.2406.1995@gmail.com>
2017-02-02 10:45:32 +0700

    first

---
+++ Bkademy/Webpos/Block/Page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anhnc
+ * Date: 02/02/2017
+ * Time: 09:18
+ */
+
+namespace Bkademy\Webpos\Block;
+
+
+class Page extends \Bkademy\Webpos\Block\AbstractBlock
+
+{
+
+}
---
+++ Bkademy/Webpos/Model/Staff.php
@@ -0,0 +1,38 @@
+<?php
+namespace Bkademy\Webpos\Model;
+
+class Staff extends \Magento\Framework\Model\AbstractModel
+{
+    protected function _construct()
+    {
+        $this->_init('Bkademy\Webpos\Model\ResourceModel\Staff');
+    }
+    /**
+     * @param $login
+     * @param $password
+     * @return bool
+     */
+    public function authenticate($username, $password) {
+        $this->loadByUsername($username);
+        if (!$this->validatePassword($password))
+            return false;
+        return true;
+    }
+    /**
+     * @param $username
+     * @return $this
+     */
+    public function loadByUsername($username) {
+        $staffs = $this->getCollection()->addFieldToFilter('username', $username);
+        if ($id = $staffs->getFirstItem()->getId())
+            $this->load($id);
+        return $this;
+    }
+    /**
+     * @param $password
+     * @return bool
+     */
+    public function validatePassword($password) {
+        return $this->getPassword() == $password ? true : false;
+    }
+}
